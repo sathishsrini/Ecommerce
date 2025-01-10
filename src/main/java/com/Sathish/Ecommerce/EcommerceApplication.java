cc2e28c005bb5c6bf5743752037ce2dd8d919b04
sathishsrini <sathishsrini499@gmail.com>
2025-01-10 16:54:40 +0530

    Fixed issues in EcommerceApplication.java and updated pom.xml

--- src/main/java/com/Sathish/Ecommerce/EcommerceApplication.java
+++ src/main/java/com/Sathish/Ecommerce/EcommerceApplication.java
@@ -2,12 +2,20 @@ package com.Sathish.Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends SpringBootServletInitializer {
 
+	// This method is for WAR deployment configuration
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EcommerceApplication.class); // Correct class name here
+	}
+
+	// Main method to start the Spring Boot application
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-
 }
